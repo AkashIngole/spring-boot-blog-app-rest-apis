d5de36e85669e67609b7280e4b8a8aafcaa5b9b5
akash214ingole@gmail.com <akash214ingole@gmail.com>
2023-04-26 22:10:21 +0530

    Created Post API

---
+++ src/main/java/com/springboot/blog/controller/PostController.java
@@ -0,0 +1,25 @@
+package com.springboot.blog.controller;
+
+import com.springboot.blog.payload.PostDto;
+import com.springboot.blog.service.PostService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostController {
+
+    private PostService postService; // using interface here for loose coupling
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    // Create blog post REST api
+    @PostMapping
+    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto) {
+        return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
+    }
+}
