d776b59ffbd5bee0c5fda7a77e966bd53bf4c739
akash214ingole@gmail.com <akash214ingole@gmail.com>
2023-05-07 20:36:47 +0530

    Developed global exception handler

--- src/main/java/com/springboot/blog/payload/CommentDto.java
+++ src/main/java/com/springboot/blog/payload/CommentDto.java
@@ -1,6 +1,9 @@
 package com.springboot.blog.payload;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +13,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CommentDto {
     private long id;
+
+    @NotEmpty(message = "Name should not be null/empty.")
     private String name;
+
+    @NotEmpty(message = "Email should not be null/empty.")
+    @Email
     private String email;
+
+    @NotEmpty(message = "Comment text cannot be null/empty.")
+    @Size(min = 10)
     private String messageBody;
 }
